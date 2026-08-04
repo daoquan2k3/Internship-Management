6afc74b94af4316ffc3525a651edf789a3987ac0
daoquan2k3 <daoanhquan2k3@gmail.com>
2026-08-04 20:50:56 +0700

    Clean up unused imports and empty lines in AdminDashboard

--- Frontend/src/pages/admin/AdminDashboard.jsx
+++ Frontend/src/pages/admin/AdminDashboard.jsx
@@ -9,7 +9,6 @@ import { AdminCharts } from "./components/AdminCharts";
 // Icons
 import GroupIcon from "@mui/icons-material/Group";
 import AssignmentIcon from "@mui/icons-material/Assignment";
-import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
 import PublicIcon from "@mui/icons-material/Public";
 
 
@@ -63,7 +62,6 @@ const AdminDashboard = () => {
       {/* 4 THẺ THỐNG KÊ */}
       <Box sx={{ display: "grid", gridTemplateColumns: "repeat(auto-fit, minmax(240px, 1fr))", gap: 3, mb: 4 }}>
         <StatCard delay={0.1} color="#f59e0b" icon={<GroupIcon fontSize="large" />} title="Tổng Người Dùng" value={stats.totalUsers.toLocaleString()} />
-
         <StatCard delay={0.3} color="#10b981" icon={<PublicIcon fontSize="large" />} title="Tổng Lượt Truy Cập" value={stats.websiteVisits.toLocaleString()} />
         <StatCard delay={0.4} color="#8b5cf6" icon={<AssignmentIcon fontSize="large" />} title="Nhóm Thực Tập" value={stats.totalAssignments.toLocaleString()} />
       </Box>
